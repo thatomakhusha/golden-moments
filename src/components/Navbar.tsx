6428d7499197950b0dfb377fbab150665b5a829d
Thato Makhusha <makhushathato2@outlook.com>
2026-07-30 21:42:30 +0200

    Improve responsive navbar and mobile menu

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -16,7 +16,7 @@ export default function Navbar() {
 
   return (
     <nav className="sticky top-0 z-50 border-b border-gold/20 bg-cream/80 backdrop-blur-xl">
-      <div className="mx-auto flex h-19.5 max-w-7xl items-center justify-between px-6 lg:px-10">
+      <div className="mx-auto flex h-20 max-w-7xl items-center justify-between px-6 lg:px-10">
 
         <Link href="/" onClick={() => setMenuOpen(false)}>
           <Image
@@ -110,6 +110,7 @@ export default function Navbar() {
 
             <Link
               href="/order"
+              onClick={() => setMenuOpen(false)}
               className="
                 rounded-full
                 border
